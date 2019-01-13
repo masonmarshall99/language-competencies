0efd171e0141022e5ffd9574b5c09451e32876b2
Mason Marshall <45390106+masonmarshall99@users.noreply.github.com>
2019-01-13 14:25:20 +0800

    Create fizzbuzz.c

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,15 @@
+//Print a sequence of numbers
+//Multiples of 3 are replaced with "fizz"
+//Multiples of 5 are replaced with "buzz"
+//Multiples of 3 & 5 are replaced with "fizzbuzz"
+//Do this for n numbers
+#include<stdio.h>
+
+int n = 100;
+
+for (int i=1; i<=n; i++) {
+	char fizz[5] = "";
+	char buzz[5] = "";
+	
+	
+}
